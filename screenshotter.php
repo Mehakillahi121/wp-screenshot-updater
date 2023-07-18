47a3c1ef3bf9ed72280c037da62842e97c54142e
Mehakillahi121 <webfort.mehak@gmail.com>
2023-07-18 15:46:36 +0530

    2.0

--- screenshotter.php
+++ screenshotter.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Screenshot-Plugin
 Description: Convert URL or HTML content into an image.
-Version: 1.0
+Version: 2.0
 Author: Mehak Illahi
 Documentation: https://your-plugin-documentation-url.com
 */
